4bd7aa464de5028af4199a257414c0a843c276f9
Nicoleta7 <nicoleta.cazac@gmail.com>
2021-06-02 11:22:00 +0300

    exam ed added

--- src/Diapazon/diapSuma.java
+++ src/Diapazon/diapSuma.java
@@ -4,7 +4,7 @@ public class diapSuma {
     public static void main(String[] args) {
 
         int sum = 0;
-    for(int i = 0; i <= 30; i++){
+    for(int i = 25; i <= 30; i++){
         sum = sum + i;
     }
         System.out.println("Sum of numbers in range 25-30(included): " + sum);
